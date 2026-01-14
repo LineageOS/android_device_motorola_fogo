e500556ece17562b0836273f7c4994d901edcd72
Michael Bestas <mkbestas@lineageos.org>
2026-01-15 01:19:54 +0200

    fogo: Migrate to MotoEuicc
    Change-Id: If0dc9370ad1588daec5b6637a4a2864417fc32b5

--- device.mk
+++ device.mk
@@ -86,5 +86,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    MotoEuicc
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/fogo/fogo-vendor.mk)
